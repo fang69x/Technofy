cf42565785a2412d673d5055d2b3ec84cb989959
KISLAY <kislayk12@gmail.com>
2023-06-11 20:35:13 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:universe7/homepage.dart';
 
 void main() {
   runApp(const Universe7());
@@ -10,13 +11,7 @@ class Universe7 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: const Text("Welcome to Universe 7 "),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
