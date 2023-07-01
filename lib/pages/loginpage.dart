28679a8a452d00e5824f8801c865879971d4ef8e
KISLAY <kislayk12@gmail.com>
2023-07-02 01:48:55 +0530

    day7 completed

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -9,93 +9,113 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-bool changebutton = false;
-
 class _LoginPageState extends State<LoginPage> {
+  bool changebutton = false;
+  final _formkey = GlobalKey<FormState>();
+  moveToHome(BuildContext context) async {
+    if (_formkey.currentState!.validate()) {
+      setState(() {
+        changebutton = true;
+      });
+      await (Future.delayed(Duration(seconds: 1)));
+      await Navigator.pushNamed(context, Myrasta.HomeRoutes);
+      setState(() {
+        changebutton = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/abstract-digital-grid-black-background.jpg",
-            fit: BoxFit.contain,
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          const Text(
-            "Welcome",
-            style: TextStyle(
-              fontSize: 34,
-              fontWeight: FontWeight.w400,
+      child: Form(
+        key: _formkey,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/abstract-digital-grid-black-background.jpg",
+              fit: BoxFit.contain,
             ),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter Username",
-                    labelText: " username",
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 34,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: " username",
+                    ),
+                    validator: (value) {
+                      if (value != null) {
+                        return "Username cannot be empty";
+                      }
+                    },
                   ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter password",
-                    labelText: " password",
+                  TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter password",
+                        labelText: " password",
+                      ),
+                      validator: (value) {
+                        if (value != null) {
+                          return "Password cannot be empty";
+                        }
+                      }),
+                  const SizedBox(
+                    height: 20.0,
                   ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                InkWell(
-                  onTap: () async {
-                    setState(() {
-                      changebutton = true;
-                    });
-                    await (Future.delayed(Duration(seconds: 1)));
-                    Navigator.pushNamed(context, Myrasta.HomeRoutes);
-                  },
-                  child: AnimatedContainer(
-                    duration: Duration(seconds: 1),
-                    height: 50,
-                    width: changebutton ? 50 : 100,
-                    alignment: Alignment.center,
-                    child: changebutton
-                        ? Icon(
-                            Icons.done,
-                            color: Colors.white24,
-                          )
-                        : Text(
-                            "login",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
+                  InkWell(
+                    onTap: () => moveToHome(context),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      height: 50,
+                      width: changebutton ? 50 : 100,
+                      alignment: Alignment.center,
+                      child: changebutton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white24,
+                            )
+                          : Text(
+                              "login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
                             ),
-                          ),
-                    decoration: BoxDecoration(
-                        color: Colors.deepPurple,
-                        borderRadius:
-                            BorderRadius.circular(changebutton ? 20 : 8)),
-                  ),
-                )
-                // ElevatedButton(
-                //   onPressed: () {
-                //     Navigator.pushNamed(context, Myrasta.HomeRoutes);
-                //   },
-                //   child: Text("LOGIN"),
-                // ),
-              ],
-            ),
-          )
-        ],
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius:
+                              BorderRadius.circular(changebutton ? 20 : 8)),
+                    ),
+                  )
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, Myrasta.HomeRoutes);
+                  //   },
+                  //   child: Text("LOGIN"),
+                  // ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
       //   child: Center(
       //       child: Text(
