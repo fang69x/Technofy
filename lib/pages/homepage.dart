07411da58740561de8ceed217b05ae611a79de88
KISLAY <kislayk12@gmail.com>
2023-07-12 22:14:24 +0530

    day 9 completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:universe7/widgets/item_widget.dart';
+
 import 'dart:convert';
 import '../models/catalog.dart';
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,18 +12,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final int days = 30;
-
-  final String name = "Codepur";
-
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     loadData();
   }
 
   loadData() async {
+    await Future.delayed(Duration(seconds: 2));
     final catalogJson =
         await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalogJson);
@@ -42,12 +39,42 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items?.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items![index],
+            // ? ListView.builder(
+            //     itemCount: CatalogModel.items?.length,
+            //     itemBuilder: (context, index) => ItemWidget(
+            //       item: CatalogModel.items![index],
+            //     ),
+            //   )
+            ? GridView.builder(
+                itemCount: CatalogModel.items
+                    ?.length, // ye vo error nahi aane dega range error jo aa jata hai
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 16,
                 ),
-              )
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items![index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    child: GridTile(
+                      header: Container(
+                        child: Text(
+                          item.name,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                        ),
+                        padding: const EdgeInsets.all(12),
+                      ),
+                      child: Image.network(item.image),
+                      footer: Text(
+                        item.price.toString(),
+                      ),
+                    ),
+                  );
+                })
             : Center(
                 child: CircularProgressIndicator(),
               ),
